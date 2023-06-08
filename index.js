c437d93fa2254e5d1f5674b2963a91b0363c3e17
Michal Moravec <michal10moravec@gmail.com>
2023-06-08 09:24:10 +0200

    Playwright init

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const { init } = require('./src/files')
 const server = require('./src/server')
+const { chromium } = require('playwright')
 
 const index = async () => {
-    await init({})
-    server()
+    // await init({})
+    // server()
+    console.log('Launching chromium')
+    const browser = await chromium.launch();
+    console.log('Chromium launched')
+    const page = await browser.newPage();
+    console.log('Page created')
+    await page.goto('https://www.google.com');
+    console.log('Visited google')
+    await browser.close()
+    console.log('Closed browser')
 }
 
 index()
